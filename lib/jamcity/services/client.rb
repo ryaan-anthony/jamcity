c958d468c5ac7d0ec92869398e384242d5ecccbe
ryaan-anthony <rtulino@gmail.com>
2020-05-17 15:16:33 -0400

    connect client

--- lib/jamcity/services/client.rb
+++ lib/jamcity/services/client.rb
@@ -3,12 +3,28 @@
 module Jamcity
   class Client
     class << self
+      def run(app)
+        @app = app
+
+        send_message('HOWDY')
+      end
+
+      private
+
       def send_message(message)
-        hostname = 'localhost'
-        port = 2000
-        socket = TCPSocket.open(hostname, port)
+        socket = TCPSocket.open(host, port)
         socket.sendmsg(message)
+        socket.close_write
+        puts socket.read
         socket.close
+      end
+
+      def host
+        @app.config.server['host']
+      end
+
+      def port
+        @app.config.server['port']
       end
     end
   end
